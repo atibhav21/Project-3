0e0d2a54ffb78d71d02aafc9a5f84e4a47bc7af9
Atibhav Mittal <atibhav@wifi-131-179-34-243.host.ucla.edu>
2015-10-19 17:11:52 -0700

    Initial Commit

---
+++ Project 3/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project 3
+//
+//  Created by Atibhav Mittal on 10/19/15.
+//  Copyright © 2015 ati. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
